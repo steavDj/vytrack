1ba6df10be926dcdd6e820669211ab6bbe29314d
Mansurmohamed <abdullamansur3@gmail.com>
2025-02-23 16:19:55 -0500

    added steps into driver login module and modulepage

--- src/test/java/com/vytrack/step_definitions/US01_MainModules_StepDefs.java
+++ src/test/java/com/vytrack/step_definitions/US01_MainModules_StepDefs.java
@@ -1,6 +1,7 @@
 package com.vytrack.step_definitions;
 
 import com.vytrack.pages.LoginPage;
+import com.vytrack.pages.MainModulesPage;
 import com.vytrack.utilities.ConfigurationReader;
 import com.vytrack.utilities.Driver;
 import io.cucumber.java.en.Given;
@@ -8,6 +9,7 @@ import io.cucumber.java.en.Then;
 
 public class US01_MainModules_StepDefs {
 
+    MainModulesPage mainModulesPage = new MainModulesPage();
     LoginPage loginPage = new LoginPage();
 
     @Given("the user is logged in as driver")
@@ -24,6 +26,7 @@ public class US01_MainModules_StepDefs {
     @Then("user should see four modules in the select bar")
     public void user_should_see_four_modules_in_the_select_bar() {
 
+        mainModulesPage.mainModules.get(0).click();
 
     }
 
