285513e0db33a1d7c6058b884a89143ac6a3bd29
Mansurmohamed <abdullamansur3@gmail.com>
2025-02-24 19:17:22 -0500

    completed driver steps in module page

--- src/test/java/com/vytrack/step_definitions/US01_MainModules_StepDefs.java
+++ src/test/java/com/vytrack/step_definitions/US01_MainModules_StepDefs.java
@@ -6,6 +6,13 @@ import com.vytrack.utilities.ConfigurationReader;
 import com.vytrack.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
 
 public class US01_MainModules_StepDefs {
 
@@ -25,9 +32,25 @@ public class US01_MainModules_StepDefs {
 
     @Then("user should see four modules in the select bar")
     public void user_should_see_four_modules_in_the_select_bar() {
+//        mainModulesPage.mainModules.get(1).click();
+//        String actualOption = mainModulesPage.mainModules.get(1).getText().trim();
+//        String expectedOption = "Customers";
+//
+//        Assert.assertEquals(expectedOption, actualOption);
 
-        mainModulesPage.mainModules.get(0).click();
+        List<String> expectedOptions = Arrays.asList("Fleet", "Customers", "Activities", "System");
 
+        for(int i = 0; i < mainModulesPage.mainModules.size(); i++){
+
+            WebElement option = mainModulesPage.mainModules.get(i);
+            option.click();
+
+            String expectedOption = expectedOptions.get(i);
+            String actualOption = option.getText().trim();
+
+            Assert.assertEquals(expectedOption, actualOption);
+
+        }
     }
 
 }
