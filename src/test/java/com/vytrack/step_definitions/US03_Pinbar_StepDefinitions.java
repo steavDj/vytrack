db346f237dd519b990bf5ba8016ad6d1f4254caa
irynas11 <soroka.v.iryna@gmail.com>
2025-02-25 21:25:20 -0600

    Updated Pinbar_StepDefs

---
+++ src/test/java/com/vytrack/step_definitions/US03_Pinbar_StepDefinitions.java
@@ -0,0 +1,45 @@
+package com.vytrack.step_definitions;
+
+import com.vytrack.pages.BasePage;
+import com.vytrack.pages.PinbarPage;
+import com.vytrack.utilities.BrowserUtils;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class US03_Pinbar_StepDefinitions {
+
+    BasePage basePage = new BasePage();
+    PinbarPage pinbarPage = new PinbarPage();
+
+    @When("user click the Learn how to use this space link on the homepage")
+    public void userClickTheLearnHowToUseThisSpaceLinkOnTheHomepage() throws InterruptedException {
+       BrowserUtils.waitForVisibility(basePage.learnPinbar, 3);
+        BrowserUtils.waitForClickablility(basePage.learnPinbar, 3);
+        basePage.learnPinbar.click();
+    }
+
+    @Then("user sees {string} as a main header")
+    public void userSeesAsAMainHeader(String expectedText) {
+        Assert.assertEquals(expectedText, pinbarPage.howToUsePinBarHeader.getText());
+
+    }
+
+    @Then("user sees {string}")
+    public void userSees(String expectedText) {
+        Assert.assertEquals(expectedText, pinbarPage.usePinIconHeader.getText());
+    }
+
+    @Then("user sees an image on the page")
+    public void userSeesAnImageOnThePage() {
+        String expectedImageSource = "/bundles/oronavigation/images/pinbar-location.jpg";
+        Assert.assertTrue(pinbarPage.pinbarImage.getAttribute("src").contains(expectedImageSource));
+        Assert.assertTrue("Image is Not Visible", pinbarPage.pinbarImage.isDisplayed());
+    }
+
+
+
+}
